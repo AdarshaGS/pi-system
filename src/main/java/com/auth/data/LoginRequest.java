5cc8aed2f8fef4f3075b2c66199ba41b4fb6387b
AdarshaGS <adarshgs1928@gmail.com>
2026-01-19 22:43:09 +0530

    changes

--- src/main/java/com/auth/data/LoginRequest.java
+++ src/main/java/com/auth/data/LoginRequest.java
@@ -1,17 +1,14 @@
 package com.auth.data;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Data
-@AllArgsConstructor
+@SuperBuilder
 @NoArgsConstructor
 public class LoginRequest {
 
     private String email;
     private String password;
-    private String confirmPassword;
-    private String newPassword;
-    private String oldPassword;
 }
